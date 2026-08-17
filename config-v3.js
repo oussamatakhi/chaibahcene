8e4ed28981fc90393c50a4d04c64d042d6b7ac34
oussamatakhi <37408253+oussamatakhi@users.noreply.github.com>
2026-08-17 01:54:15 +0100

    إضافة زر إغلاق سفلي للنوافذ المنبثقة مع الحفاظ على زر X العلوي

--- config-v3.js
+++ config-v3.js
@@ -7,22 +7,27 @@ window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_t9YsG10SgXDyKPANd6TdCA_UosY7Yz
     window.__ministerialOpenReport = window.openReport;
 
     var app=document.createElement('script');
-    app.src='app-fix.js?v=20260817-5';
+    app.src='app-fix.js?v=20260817-6';
     app.onload=function(){
       var finalFix=document.createElement('script');
-      finalFix.src='report-final.js?v=20260817-3';
+      finalFix.src='report-final.js?v=20260817-4';
       finalFix.onload=function(){
         var visitFix=document.createElement('script');
-        visitFix.src='visit-teacher-search.js?v=20260817-2';
+        visitFix.src='visit-teacher-search.js?v=20260817-3';
         visitFix.onload=function(){
           var autofill=document.createElement('script');
-          autofill.src='report-autofill-v2.js?v=20260817-2';
+          autofill.src='report-autofill-v2.js?v=20260817-3';
           autofill.onload=function(){
             var textFix=document.createElement('script');
-            textFix.src='report-text-corrections.js?v=20260817-2';
+            textFix.src='report-text-corrections.js?v=20260817-3';
             textFix.onload=function(){
               var finalTextFix=document.createElement('script');
-              finalTextFix.src='report-text-corrections-v2.js?v=20260817-1';
+              finalTextFix.src='report-text-corrections-v2.js?v=20260817-2';
+              finalTextFix.onload=function(){
+                var modalFix=document.createElement('script');
+                modalFix.src='modal-bottom-close.js?v=20260817-1';
+                document.head.appendChild(modalFix);
+              };
               document.head.appendChild(finalTextFix);
             };
             document.head.appendChild(textFix);
